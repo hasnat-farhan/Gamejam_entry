27351892e59917c0c18d47a1b43ae4e0bbe1d8ee
kn8trix <kn8trix00124@gmail.com>
2026-07-20 20:14:30 +0600

    fix: res, spawn, camera, tile order
    - project.godot: 640x360 viewport with 1280x720 window (2x integer scale for crisp 720p)
    - node_2d.tscn: reorder Entities after TileMapLayers so player renders above tiles
    - node_2d.tscn: set P1 spawn at Vector2(320, 240)
    - game_camera.gd: enable smoothing from start, safe null-guard on target

--- scenes/system/game_camera.gd
+++ scenes/system/game_camera.gd
@@ -5,20 +5,13 @@ extends Camera2D
 @export var target_manager: TargetManager
 
 func _ready() -> void:
-	_enable_smoothing(false)
-	target_manager.target_reached.connect(_init_camera)
+	position_smoothing_enabled = true
+	position_smoothing_speed = 5.0
 
-func _physics_process(_delta: float) -> void:
+func _process(_delta: float) -> void:
 	_follow_target()
 
-##internal - When transitioning between levels, the camera will be activated upon completing the transfer.
-func _init_camera():
-	_enable_smoothing(true)
-
-func _enable_smoothing(value):
-	position_smoothing_enabled = value
-		
 ##internal - Manages camera tracking of the assigned target.
 func _follow_target():
-	if target_manager:
+	if target_manager and target_manager.target:
 		global_position = target_manager.get_target_position()
